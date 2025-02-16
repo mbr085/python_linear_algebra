5ad58b3b3ab7d511938fe222592315815680f3c0
Morten Brun <mbr085@gmail.com>
2025-02-16 22:05:32 +0100

    med inhomogene diffligninger

--- src/python_linear_algebra/diffligninger.py
+++ src/python_linear_algebra/diffligninger.py
@@ -1,6 +1,26 @@
 from .matrix_reduction import finn_egenvektorer_og_egenverdier
 from .matrix_reduction import invers_matrise
+from .matrix_reduction import partikulaer_losning
 import numpy as np
+
+def løs_inhomogent_system_av_differensialligninger(overgangsmatrise, starttilstand, påtrykk, tverdier):
+    '''
+    Løser et homogent lineært system av differensialligninger analytisk
+    
+    Parametre:
+        overgangsmatrise (np.ndarray): En kvadratisk Numpy-array med koeffisientene for systemet
+        starttilstand (np.ndarray): En 1D Numpy-array av samme lengde som overgangsmatrise
+        tverdier (np.ndarray): En 1D Numpy-array med verdier av t der løsningen skal beregnes
+    
+    Retunerer:
+        y (np.ndarray): Verdiene av løsningen til sistemet i de angitte tverdier
+    '''
+    påtrykk = np.array(påtrykk).ravel()
+    starttilstand = np.array(starttilstand).ravel()
+    ligevektsløsning = partikulaer_losning(overgangsmatrise, påtrykk).ravel()
+    starttilstand = starttilstand - ligevektsløsning
+
+    return løs_homogent_system_av_differensialligninger(overgangsmatrise, starttilstand, tverdier) + ligevektsløsning[:, None]
 
 def løs_homogent_system_av_differensialligninger(overgangsmatrise, starttilstand, tverdier):
     '''
