082b268a76a024d6ab8df544ba8bf6c839f5d7b1
Morten Brun <mbr085@gmail.com>
2025-02-18 15:45:21 +0100

    med Euler

--- src/python_linear_algebra/diffligninger.py
+++ src/python_linear_algebra/diffligninger.py
@@ -44,3 +44,42 @@ def løs_homogent_system_av_differensialligninger(overgangsmatrise, starttilstan
     egenverdier = np.array([[x[0]] for x in egenvektorer_og_egenverdier])
     P = np.hstack([x[2][0] for x in egenvektorer_og_egenverdier])
     return P @ ((invers_matrise(P) @ starttilstand[:, None]) * np.exp(tverdier[None, :] * egenverdier))
+
+def eulers_metode(f, g, t0, tn, x0, y0, n):
+  """
+  Anvender Eulers metode for å løse differensialligningen x' = f(t, x, y) og y' = g(t, x, y).
+
+  Args:
+    f: Funksjonen som representerer x-delen av høyre side av differensialligningen.
+    g: Funksjonen som representerer y-delen av høyre side av differensialligningen.
+    t0: Startverdien for t.
+    tn: Sluttverdien for t.
+    x0: Startverdien for x.
+    y0: Startverdien for y.
+    n: Antall steg.
+
+  Returns:
+    En NumPy array som inneholder de approksimerte verdiene for x i hvert steg,
+    En NumPy array som inneholder de approksimerte verdiene for y i hvert steg,
+  """
+
+  # Lager en array med t-verdier fra t0 til tn med n+1 punkter
+  t_values = np.linspace(t0, tn, n + 1)
+  # Initialiserer listene for x- og y-verdiene med startverdiene
+  x_values = [x0]
+  y_values = [y0]
+  # Beregner steglengden
+  delta_t = t_values[1] - t_values[0]
+
+  # Gjennomfører Eulers metode for hvert steg
+  for i in range(n):
+    # Beregner neste x-verdi
+    x_next = x_values[-1] + delta_t * f(t_values[i], x_values[-1], y_values[-1])
+    # Beregner neste y-verdi
+    y_next = y_values[-1] + delta_t * g(t_values[i], x_values[-1], y_values[-1])
+    # Legger til de nye verdiene i listene
+    x_values.append(x_next)
+    y_values.append(y_next)
+
+  # Returnerer t-verdiene og de beregnede x- og y-verdiene som NumPy arrays
+  return t_values, np.array(x_values), np.array(y_values)
